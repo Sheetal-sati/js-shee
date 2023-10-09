3e87fd41bbaa791ff41e9c4339618f38158f3e8c
Sheetal-sati <102944430+Sheetal-sati@users.noreply.github.com>
2023-10-09 10:12:35 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const account_Id = 1451;
+let account_email = "123@gmil.com";
+var accountPass = "12234";
+accountCity = "Jaipur"; 
+
+// account_Id = 2 cannot change
+
+console.log(account_Id);
+
+account_email = "asdasd";
+
+console.log(account_email);
+
+accountPass = "aaaa";
+
+console.log(accountPass);
+
+accountCity = "jaipur";
+var my_name;
+
+console.log(accountCity);
+
+console.table([accountCity, accountPass, account_email, my_name])
+
+/*
+never use var
+*/
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict";   //treat all JS code as newer version
+
+//alert(3+3); // this will work in broswer console but node.js has different of writing alert
+
+let name = "hitesh"
+let age = 30
+let isLog = false
+
+//number => 2 to power 53
+//null => standalone value; saying intentionally the value is empty
+//undefined => might define in future
+//symbol => used to define if unique 
+
+console.log(typeof null); 
+// why null is an object?
